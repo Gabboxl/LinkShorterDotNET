c308399bd7c95da231df0375aaeb660812c34873
Gabboxl <gabboxl@virgilio.it>
2018-04-24 19:07:09 +0200

    fix adfly method and constructor

--- LinkShorterNET/class-linkshorter.cs
+++ LinkShorterNET/class-linkshorter.cs
@@ -1,13 +1,55 @@
 ﻿using System;
+using System.IO;
+using System.Net;
 
 namespace LinkShorterNET
 {
-    public class linkshorter
+    public class Linkshorter
     {
+        private string response;
+
+        public Linkshorter(string service, string url)
+        {
+            if (service == "adfly")
+            {
+                this.adfly(url);
+            }
+
+        }
+
+
+        private string getLink()
+        {
+            return response;
+        }
+
+
 
         private void adfly(string url)
         {
-            //fixed
+
+            string link = "http://api.adf.ly/api.php?key=3f885db2f68d068a6015d4f132bf37b8&uid=16175835&url=" + url;
+
+            WebRequest wrGETURL;  //system dot-net
+            wrGETURL = WebRequest.Create(link);
+
+            Stream objStream;  //system io
+            objStream = wrGETURL.GetResponse().GetResponseStream();
+
+            StreamReader objReader = new StreamReader(objStream);
+
+            string sLine = "";
+            int i = 0;
+
+
+
+            while (sLine != null)
+            {
+                i++;
+                sLine = objReader.ReadLine();
+                if (sLine != null)
+                    this.response = sLine;
+            }
         }
 
 
