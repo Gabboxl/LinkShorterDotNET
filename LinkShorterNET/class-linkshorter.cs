3f25839b726a5470ece04b11d0eb92a41fc2ab61
Gabboxl <gabboxl@virgilio.it>
2018-04-28 13:54:55 +0200

    fix

--- LinkShorterNET/class-linkshorter.cs
+++ LinkShorterNET/class-linkshorter.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Linq;
 using System.Net;
 using System.Reflection;
-using System.Net.Http;
 
 namespace LinkShorterNET
 {
@@ -14,7 +13,7 @@ namespace LinkShorterNET
         public Linkshorter(string service, string url)
         {
             
-                System.Type thisType = this.GetType();
+                Type thisType = this.GetType();
                 MethodInfo theMethod = thisType.GetMethod(service);
                 
 
@@ -39,7 +38,7 @@ namespace LinkShorterNET
         {
             string api = "http://api.adf.ly/api.php?key=3f885db2f68d068a6015d4f132bf37b8&uid=16175835&url=" + url;
 
-            using (var client = new System.Net.WebClient())
+            using (var client = new WebClient())
             {
                 this.response = client.DownloadString(api);
             }
@@ -51,7 +50,7 @@ namespace LinkShorterNET
 
             string api = "http://adfoc.us/api/?key=66ebaa219a97eac02d8690addc1d8a4e&url=" + url;
 
-            using (var client = new System.Net.WebClient())
+            using (var client = new WebClient())
             {
                 this.response = client.DownloadString(api);
             }
