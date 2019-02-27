aed270825bdbc6acc7b46c2ebc63c5ee16ace08a
Gabriele <gabboxl0@gmail.com>
2019-02-27 22:33:17 +0100

    fixx

--- makedll.sh
+++ makedll.sh
@@ -8,6 +8,7 @@ eval "$(ssh-agent -s)"
 echo -e "$private_key" > travis_rsa
 chmod 600 travis_rsa
 ssh-add travis_rsa
+
 git clone git@github.com:Gabboxl/LinkShorterDotnetDLL
 cp LinkShorterDotNET/LinkShorterNET/bin/Release/netstandard2.0/LinkShorterDotNET.dll LinkShorterDotnetDLL/
 git add -A
