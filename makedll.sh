f3a1f48db1864405b1b9d850c09350d2ef29e2de
Gabriele <gabboxl0@gmail.com>
2019-02-27 22:03:44 +0100

    Create makedll.sh
    script to integrate with travisCI

---
+++ makedll.sh
@@ -0,0 +1,14 @@
+dotnet restore
+dotnet build -c release
+
+cd ..
+
+eval "$(ssh-agent -s)"
+echo -e "$private_key" > travis_rsa
+chmod 600 travis_rsa
+ssh-add travis_rsa
+git clone git@github.com:Gabboxl/LinkShorterDotnetDLL
+cp LinkShorterDotNET/LinkShorterNET/bin/Release/netstandard2.0/LinkShorterDotNET.dll LinkShorterDotnetDLL/
+git add -A
+git commit -am "$TRAVIS_COMMIT_MESSAGE"
+git push origin master
