133d70d7e19e1fefda7fe04ed2aa1864571338aa
Gabriele <gabboxl0@gmail.com>
2019-02-28 18:08:38 +0100

    Update makedll.sh

--- makedll.sh
+++ makedll.sh
@@ -9,8 +9,7 @@ echo -e $private_key > travis_rsa
 chmod 600 travis_rsa
 ssh-add travis_rsa
 
-
-git clone git@github.com:Gabboxl/LinkShorterDotnetDLL
+git clone git@github.com:Gabboxl/LinkShorterDotnetDLL.git
 cp LinkShorterDotNET/LinkShorterNET/bin/Release/netstandard2.0/LinkShorterDotNET.dll LinkShorterDotnetDLL/
 git add -A
 git commit -am "$TRAVIS_COMMIT_MESSAGE"
