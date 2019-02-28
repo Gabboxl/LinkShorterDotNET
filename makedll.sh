328bb9f9f45d4d37a72c3964cddc6afe87ba3e27
Gabriele <gabboxl0@gmail.com>
2019-02-28 18:29:50 +0100

    fixs

--- makedll.sh
+++ makedll.sh
@@ -9,7 +9,7 @@ chmod 600 travis_rsa
 ssh-add travis_rsa
 
 git clone git@github.com:Gabboxl/LinkShorterDotnetDLL
-cp Gabboxl/LinkShorterDotNET/LinkShorterNET/bin/Release/netstandard2.0/LinkShorterDotNET.dll LinkShorterDotnetDLL/
+cp LinkShorterNET/bin/Release/netstandard2.0/LinkShorterDotNET.dll LinkShorterDotnetDLL/
 git add -A
 git commit -am "$TRAVIS_COMMIT_MESSAGE"
 git push origin master
