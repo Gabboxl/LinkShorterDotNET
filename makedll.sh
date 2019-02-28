81cd24f8625f28fa163d268dbc99523660980d16
Gabriele <gabboxl0@gmail.com>
2019-02-28 18:12:11 +0100

    Update makedll.sh

--- makedll.sh
+++ makedll.sh
@@ -3,14 +3,15 @@ dotnet restore
 dotnet build -c release
 
 cd ..
+cd ..
 
 eval "$(ssh-agent -s)"
 echo -e $private_key > travis_rsa
 chmod 600 travis_rsa
 ssh-add travis_rsa
 
-git clone git@github.com:Gabboxl/LinkShorterDotnetDLL.git
-cp LinkShorterDotNET/LinkShorterNET/bin/Release/netstandard2.0/LinkShorterDotNET.dll LinkShorterDotnetDLL/
+git clone git@github.com:Gabboxl/LinkShorterDotnetDLL
+cp Gabboxl/LinkShorterDotNET/LinkShorterNET/bin/Release/netstandard2.0/LinkShorterDotNET.dll LinkShorterDotnetDLL/
 git add -A
 git commit -am "$TRAVIS_COMMIT_MESSAGE"
 git push origin master
